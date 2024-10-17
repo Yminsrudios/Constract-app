c643b249d03ba5c53fd4c9e18acc7e749b0e75b6
Tyruver <166168831+Yminsrudios@users.noreply.github.com>
2024-10-17 19:47:01 +0300

    Update sketch.js

--- Resisted_dragon_2024_10_17_11_49_23/sketch.js
+++ Resisted_dragon_2024_10_17_11_49_23/sketch.js
@@ -1,17 +1,46 @@
-var a = 100;
+var a = 1;
 function setup() {
-  createCanvas(400, 800);
+  createCanvas(400, 650);
 }
 
 function draw() {
-  background(220);
-  fill(255);
-  rect(a,100,50,50);
+  background(100);
+  fill(199);
+  rect(1,1,500,50);
   noFill();
-  if(mouseIsPressed){
-    background(200,100,20);
+  
+  fill(199);
+  rect(1,610,500,50);
+  noFill();
+  
+  fill(230);
+  textSize(40);
+  text("Project",100,40);
+  noFill();
+  
+  fill(230);
+  rect(10,100,380,50);
+  noFill();
+  
+  if( mouseX >= 5 && mouseX <= 391 && mouseY >= 10 && mouseY <= 140 && mouseIsPressed == true ){
+  a += 2;
   }
-  fill(100,200,0);
-  rect(100,50,50,50);
+  if(a > 2){
+      rede();
+    }
+}
+function rede(){
+  background(100);
+  fill(199);
+  rect(1,1,500,50);
+  noFill();
+  
+  fill(199);
+  rect(1,450,500,850);
+  noFill();
+  
+  fill(0,0,200);
+  textSize(40);
+  text("back", 45 ,45);
   noFill();
 }
